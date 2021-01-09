768b03414ab7ab12fa41a9dec5fa66455b264bed
Mongolian <abe546@live.com>
2021-01-09 16:43:54 -0500

    Time: 0 ms (100.00%), Space: 37.4 MB (31.86%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,44 +1,44 @@
 class Solution {
-    private List<Integer> answer = new LinkedList(); 
     public List<Integer> spiralOrder(int[][] matrix) {
-        
-        int up = 0; 
-        int down = matrix.length - 1;
-        int right = matrix[0].length -1; 
-        int left = 0;
-        
-        int entry = 1; 
-        
-        while(entry <= matrix[0].length * matrix.length)
-        { 
-            //GO RIGHT
-            for(int i = up; i <= right; i++)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-           
-                
-                
-                answer.add(matrix[up][i]); 
-                entry++;
-            }
- 
-            up++;
-            //GO DOWN
-            for(int i = up; i <= down; i++)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-                
-                answer.add(matrix[i][right]); 
-                entry++;
+        LinkedList<Integer> list = new LinkedList();
 ​
-            }
+        if(matrix.length == 2 && matrix[0].length ==1)
+        {
+            list.add(matrix[0][0]);
+            list.add(matrix[1][0]);
+            return list;
+        }
+        
+        if(matrix.length == 0)
+        {
+            return Collections.EMPTY_LIST;
+        }
+​
        
-            right--;
-            
-            //GO LEFT
+​
+            for (int i = 0; i < 1; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    list.add(matrix[i][j]);
+                }
+      
+​
+            //Rotate the entire matrix system 90 degrees to the left
+​
+            int[][] tmpArray = rotateArray90DegreesToTheLeft(matrix);
+​
+            list.addAll(spiralOrder(tmpArray));
+        }
+​
+         return list;
+    }
+    
+    public int[][] rotateArray90DegreesToTheLeft(int[][] matrix) {
+​
+        int[][] tmpArray = new int[matrix[0].length][matrix.length-1];
+​
+        for(int j=matrix[0].length -1; j>=0; j--)
+        {
+            for(int i=1; i<matrix.length; i++)
+            {
+                tmpArray[ matrix[0].length - 1  - j][i-1] = matrix[i][j];
+            }
