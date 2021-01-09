43caa4f63cab2d4b0d49bddc3a44a4a86d4807ad
Mongolian <abe546@live.com>
2021-01-09 17:10:12 -0500

    Time: 0 ms (100.00%), Space: 37.1 MB (62.86%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,3 +1,42 @@
+        int down = matrix.length - 1;
+        int right = matrix[0].length -1; 
+        int left = 0;
+        
+        int entry = 1; 
+        
+        while(entry <= matrix[0].length * matrix.length)
+        { 
+            //GO RIGHT
+            for(int i = up; i <= right; i++)
+            {
+                if(entry > matrix[0].length * matrix.length)
+                {
+                    break;
+                }
+           
+                
+                
+                answer.add(matrix[up][i]); 
+                entry++;
+            }
+ 
+            up++;
+            //GO DOWN
+            for(int i = up; i <= down; i++)
+            {
+                if(entry > matrix[0].length * matrix.length)
+                {
+                    break;
+                }
+                
+                answer.add(matrix[i][right]); 
+                entry++;
+​
+            }
+       
+            right--;
+            
+            //GO LEFT
             for(int i = right; i >= left; i--)
             {
                 if(entry > matrix[0].length * matrix.length)
@@ -18,31 +57,3 @@
                 {
                     break;
                 }
-                
-                answer.add(matrix[i][left]);
-                entry++;
-            }
-            
-            left++;
-        }
-  
-        
-        return answer; 
-        
-    }
-    
-    public int[][] rotateArray90DegreesToTheLeft(int[][] matrix) {
-​
-        int[][] tmpArray = new int[matrix[0].length][matrix.length-1];
-​
-        for(int j=matrix[0].length -1; j>=0; j--)
-        {
-            for(int i=1; i<matrix.length; i++)
-            {
-                tmpArray[ matrix[0].length - 1  - j][i-1] = matrix[i][j];
-            }
-        }
-​
-        return tmpArray;
-    }
-}
