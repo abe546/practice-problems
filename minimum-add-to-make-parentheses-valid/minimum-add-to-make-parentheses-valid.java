02dcb726d9d0167f2cfda666cd729e52bc49c34a
Mongolian <abe546@live.com>
2021-01-11 16:58:27 -0500

    Time: 2 ms (6.67%), Space: 38.4 MB (8.84%) - LeetHub

---
+++ minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+        Stack<Character> left = new Stack(); 
+        
+        int issues = 0; 
+        
+        for(int i = 0; i < S.length(); i++)
+        {
+            char item = S.charAt(i); 
+            
+            if(item == '(')
+            {
+                left.push(item);
+            }else
+            {
+                if(!left.isEmpty())
+                {
+                    left.pop();
+                }else
+                {
+                    issues++;
+                }
+            }
+        }
+        
+        while(!left.isEmpty())
+        {
+            left.pop();
+            issues++;
+        }
+        
+        return issues; 
