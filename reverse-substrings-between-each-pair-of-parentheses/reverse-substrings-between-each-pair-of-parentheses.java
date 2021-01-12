f90cfad5d2f27750e3504c0750296abfd769bec7
Mongolian <abe546@live.com>
2021-01-12 00:17:58 -0500

    Time: 0 ms (100.00%), Space: 37.4 MB (72.88%) - LeetHub

---
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        StringBuilder reverseString = new StringBuilder();
+        
+           int last = s.lastIndexOf(")");
+        int first = s.indexOf("(");
+        
+        reverseStringBetweenParens(s, reverseString, 0, first, last);
+        
+     
+        return reverseString.reverse().toString();
+    }
+    
+    public int reverseStringBetweenParens(String input, StringBuilder reverseString, int index, int first, int last)
+    {
+        StringBuilder stringBuilder = new StringBuilder(); 
+        
+        for(int i = index; i < input.length(); i++)
+        {
+            char item = input.charAt(i);
+            
+            if(item == '(')
+            {  
+               i = reverseStringBetweenParens(input, stringBuilder, i+1, first, last);
+            }else if(item != '(' && item != ')')
+            {
+                stringBuilder.append(item); 
+            }else if(item == ')')
+            {
+                //break out of the loop
+                //skipAhead
+              
+                    reverseString.append(stringBuilder.reverse().toString());
+                return i;
+            }
+        }
+        
+        reverseString.append(stringBuilder.reverse().toString()); 
+        
+        return input.length(); 
+    
+    }
+}
