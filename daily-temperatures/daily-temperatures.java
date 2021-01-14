e5a80a1714164ee3336908d7ef61f475ad17debe
Mongolian <abe546@live.com>
2021-01-14 11:33:35 -0500

    Time: 14 ms (74.25%), Space: 47.9 MB (20.34%) - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] dailyTemperatures(int[] T) {
-    
-        int[] answer = new int[T.length]; 
-        Stack<Integer> indexes = new Stack();         
+      int[] answer = new int[T.length];
+        
+        Stack<Integer> indexes = new Stack();
+        
         for(int i = 0; i < T.length; i++)
         {
             if(i+1 < T.length && T[i] < T[i+1] && indexes.isEmpty())
             {
-                answer[i] = 1; 
+                answer[i] = 1;
             }else
             {
-                //As long as we don't have any entry in the stack that is less than T[i] we cant do
-                //Anything, so we push
-                //The moment we find a T[i] > last index added we will be popping as long as the 
-                // value of indexes.peek < T[i]
-                while(!indexes.isEmpty() && T[i] > T[indexes.peek()])
+                while(!indexes.isEmpty() && T[indexes.peek()] < T[i])
                 {
-                    int index = indexes.pop(); 
-                    answer[index] = i - index; 
+                    int index = indexes.pop();
+                    answer[index] = i - index;
                 }
-                indexes.push(i); 
+                indexes.push(i);
             }
         }
  
