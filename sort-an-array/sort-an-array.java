db9256a0b63f76786a2aac24f557ac55f6d63afe
Mongolian <abe546@live.com>
2021-01-23 15:07:52 -0500

    Time: 6 ms (55.77%), Space: 48 MB (15.66%) - LeetHub

--- sort-an-array/sort-an-array.java
+++ sort-an-array/sort-an-array.java
@@ -1,34 +1,51 @@
-        int i=0; 
-    int j=0; 
-    int k=0; 
-    
-    while(i<left.length && j<right.length)
-    {
-      if(left[i]>right[j])
-      {
-        nums[k] = right[j];
-        j++;
-        k++;
-      }else
-      {
-        nums[k] = left[i];
-        i++;
-        k++;
-      }
+        }
+        
+        int[] right = new int[nums.length-mid];
+        
+        for(int i = mid; i < nums.length; i++)
+        {
+             
+            right[i - mid] = nums[i];
+        }
+        
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, nums);
     }
     
-    while(i<left.length)
+    public void merge(int[] left, int[] right, int[] nums)
     {
-      nums[k] = left[i];
-      i++; 
-      k++;
-    }
-    
-    while(j<right.length)
-    {
-      nums[k] = right[j];
-      j++; 
-      k++; 
-    }
+        int i = 0; 
+        int j = 0;
+        int k = 0; 
+        
+        while(i < left.length && j < right.length)
+        {
+            if(left[i] > right[j])
+            {
+                nums[k] = right[j];
+                j++;
+                k++;
+            }else 
+            {
+                nums[k] = left[i];
+                k++;
+                i++;
+            }
+        }
+        
+        while(i < left.length)
+        {
+            nums[k] = left[i];
+            k++;
+            i++;
+        }
+        
+        while(j < right.length)
+        {
+            nums[k] = right[j];
+            k++;
+            j++;
+        }
     }
 }
