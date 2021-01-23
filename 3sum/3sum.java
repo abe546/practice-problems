6ce71dd38f7d650f6b2b70c5461b5633f98ae1ff
Mongolian <abe546@live.com>
2021-01-23 01:54:42 -0500

    Time: 320 ms (22.36%), Space: 45.5 MB (14.97%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,4 +1,39 @@
+class Solution {
+    
+    
+    Map<Integer, Integer> map = new HashMap();
+    Set<List<Integer>> set = new HashSet(); 
+    Set<Integer> ignoreDupe = new HashSet(); 
+    public List<List<Integer>> threeSum(int[] nums) {
+ 
         
+        for(int i =0; i < nums.length; i++)
+        {
+            map.put(nums[i], i);
+        }
+        
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            int item = nums[i];
+            if(!ignoreDupe.contains(item)){
+            getThreeSum(nums, -1 * item, i);
+                ignoreDupe.add(item); 
+            }
+        }
+        
+        List<List<Integer>> answer = new LinkedList(); 
+        
+        for(List<Integer> item : set)
+        {
+            answer.add(item); 
+        }
+        
+        return answer;
+    }
+    
+    public void getThreeSum(int[] nums, int target, int index)
+    {
         for(int i = 0; i < nums.length; i++)
         {
             if( i == index )
