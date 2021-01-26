9d85299badc0740848b03965f12b2a9151f27a56
Mongolian <abe546@live.com>
2021-01-26 15:24:49 -0500

    Time: 14 ms (5.04%), Space: 39.3 MB (16.86%) - LeetHub

--- reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,40 +1,41 @@
 class Solution {
     public String reverseParentheses(String s) {
+        
         StringBuilder reverseString = new StringBuilder();
         
-           int last = s.lastIndexOf(")");
-        int first = s.indexOf("(");
         
-        reverseStringBetweenParens(s, reverseString, 0, first, last);
+        reverseStringBetweenParens(s, reverseString, 0);
         
      
         return reverseString.toString();
     }
     
-    public int reverseStringBetweenParens(String input, StringBuilder reverseString, int index, int first, int last)
-    {
-        StringBuilder stringBuilder = new StringBuilder(); 
-        
-        for(int i = index; i < input.length(); i++)
-        {
-            char item = input.charAt(i);
-            
-            if(item == '(')
-            {  
-               i = reverseStringBetweenParens(input, stringBuilder, i+1, first, last);
-            }else if(item != '(' && item != ')')
-            {
-                stringBuilder.append(item); 
-            }else if(item == ')')
-            {
-                //break out of the loop
-                //skipAhead
-              
-                    reverseString.append(stringBuilder.reverse().toString());
-                return i;
-            }
-        }
-        
-        reverseString.append(stringBuilder.toString()); 
+    public int reverseStringBetweenParens(String input, StringBuilder reverseString, int index)
+    {        
+       for(int i = index; i < input.length(); i++)
+       {
+           char item = input.charAt(i); 
+           System.out.println("ITEM : "+item); 
+           if(Character.isLetter(item))
+           {
+               reverseString.append(item);
+           }else if(item == '(')
+           {
+               StringBuilder tmp = new StringBuilder(); 
+               
+               i = reverseStringBetweenParens(input, tmp, i+1);
+               reverseString.append(tmp); 
+           }else if(item == ')')
+           {
+               reverseString = reverseString.reverse();
+               return i; 
+           }
+       }
         
         return input.length(); 
+    }
+              
+  
+    
+  
+}
