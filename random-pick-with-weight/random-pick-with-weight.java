4ff88c8bad388241eb22399c7d56f926c69a333b
Mongolian <abe546@live.com>
2021-01-27 23:55:18 -0500

    Time: 55 ms (22.26%), Space: 57.6 MB (5.15%) - LeetHub

--- random-pick-with-weight/random-pick-with-weight.java
+++ random-pick-with-weight/random-pick-with-weight.java
@@ -1,27 +1,43 @@
 class Solution {
-    private int[] prefixSums;
-    private int totalSum;
-​
-    public Solution(int[] w) {
-        this.prefixSums = new int[w.length];
-​
-        int prefixSum = 0;
-        for (int i = 0; i < w.length; ++i) {
-            prefixSum += w[i];
-            this.prefixSums[i] = prefixSum;
-        }
-        this.totalSum = prefixSum;
-    }
-​
-    public int pickIndex() {
-        double target = this.totalSum * Math.random();
-        int i = 0;
-        // run a linear search to find the target zone
-        for (; i < this.prefixSums.length; ++i) {
-            if (target < this.prefixSums[i])
-                return i;
-        }
-        // to have a return statement, though this should never happen.
-        return i - 1;
-  }
+
+    private TreeMap<Double, Integer> prefixSums = new TreeMap(); 
+    private double totalSum = 0; 
+    private int lastIndex = 0; 
+    
+    public Solution(int[] w) {
+        
+        int sum = 0; 
+        
+        for(int i = 0; i < w.length; i++)
+        {
+            sum+=w[i]; 
+            
+            prefixSums.put(sum*1.0, i); 
+            lastIndex = i;
+        }
+        
+        totalSum = sum;
+    }
+    
+    public int pickIndex() {
+        
+        double target = totalSum * Math.random();
+        
+        Double key = prefixSums.ceilingKey(target); 
+        
+        if(key == null)
+        {
+            return lastIndex;
+        }
+        
+        return prefixSums.getOrDefault(key, lastIndex);
+        
+        
+    }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
