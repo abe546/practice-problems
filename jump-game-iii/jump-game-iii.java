ed20ea329332f25a23ea070094edc25431f02861
Mongolian <abe546@live.com>
2021-02-14 22:33:21 -0500

    Time: 8 ms (16.45%), Space: 298.6 MB (5.07%) - LeetHub

---
+++ jump-game-iii/jump-game-iii.java
@@ -0,0 +1,52 @@
+class Solution {
+    private Map<Integer, Boolean> cache = new HashMap();
+    
+    public boolean canReach(int[] arr, int start) {
+        
+        //Idea is to explore i + arr[i] and i - arr[i] as long as the value is greater
+        //than 0 and less than the size of the array
+        //We will jump around, and as we do we will take note of the results of the 
+        //given index and save it to a map in case we encounter this same value again
+        
+        return jumpAround(arr, start);
+    }
+    
+    public boolean jumpAround(int[] arr, int start)
+    {        
+        if(start >= arr.length || start < 0)
+        {
+            return false;
+        }
+        
+        if(arr[start] == 0)
+        { 
+            return true; 
+        }
+         
+        if(cache.get(start) != null)
+        {
+            return cache.get(start); 
+        }
+        
+        int optionA = start + arr[start];
+        int optionB = start - arr[start];
+       
+        if(cache.get(start) == null || cache.get(start) == false)
+        {
+            cache.put(start, jumpAround(arr, optionA)); 
+            
+            if(cache.get(start) == false)
+            {
+                cache.put(start,   jumpAround(arr, optionB)); 
+            }
+        }
+        
+        return cache.get(start); 
+        
+    }
+    
+    public String format(int index, int value)
+    {
+        return String.format("%s:%s", index, value); 
+    }
+}
