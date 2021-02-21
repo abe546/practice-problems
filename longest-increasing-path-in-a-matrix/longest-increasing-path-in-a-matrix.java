3d9bdf0f9e64eadf9eff4facf73ac0b77d82a157
Mongolian <abe546@live.com>
2021-02-20 22:59:39 -0500

    Time: 10 ms (43.36%), Space: 39.5 MB (51.92%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,48 +1,53 @@
 class Solution {
-    public   int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
-    public int longestIncreasingPath(int[][] matrix) {
-         
-        if(matrix == null || matrix.length == 0)
-        {
-            return 0; 
-        }
- 
-        int[][] visited = new int[matrix.length][matrix[0].length];
-    
-        int answer = 0; 
-        for(int i=0; i< matrix.length; i++)
-        {
-            for(int j=0; j< matrix[0].length; j++)
-            { 
-                answer = Math.max(answer, explore(matrix, i, j, visited, 1));
-            
-            }
-        }
-        
-        return answer; 
-        
-    }
-    
-    public int explore(int[][] matrix, int i, int j, int[][] visited, int level)
-    {
-        if(visited[i][j] != 0)
-        {
-            return visited[i][j];
-        }
-        
-         
-         
-        for(int[] ijIndex : directions)
-        {
-            int a = i+ijIndex[0]; 
-            int b = j+ijIndex[1];
-            if(a >=0 && a < matrix.length && b >=0 && b < matrix[0].length && matrix[a][b] > matrix[i][j]){
-            visited[i][j] = Math.max(visited[i][j], explore(matrix, a, b, visited, level)); 
-            }
-        }
-     
-        visited[i][j] +=1;
-        return visited[i][j]; 
-        
-    }
+    
+    private int longestPath = 0;
+    private int[][] directions = 
+    {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1}
+    };
+    
+    private int[][] memoryPath;
+    
+    public int longestIncreasingPath(int[][] matrix) {
+     
+        memoryPath = new int[matrix.length][matrix[0].length];
+    
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix[0].length; j++)
+            {
+                longestPath = Math.max(longestPath, traverseMatrix(matrix, i, j, 0));
+            }
+        }
+        
+        return longestPath; 
+    }
+   
+    public int traverseMatrix(int[][] matrix, int i, int j, int level)
+    {
+        if(memoryPath[i][j] != 0)
+        {
+            return memoryPath[i][j]; 
+        }
+        
+        for(int[] dir : directions)
+        {
+            int x = i + dir[0]; 
+            int y = j + dir[1];
+            
+            if(x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && matrix[x][y] > matrix[i][j])
+            {  
+                memoryPath[i][j] = Math.max(memoryPath[i][j], traverseMatrix(matrix, x, y, level+1)); 
+            
+            }
+        }
+        
+        memoryPath[i][j] += 1; 
+        
+        return memoryPath[i][j]; 
+    }
+    
 }
