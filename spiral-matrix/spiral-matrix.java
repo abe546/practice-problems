fc0ff77a54bf0ff24589a0d583e5a5ef0c579aa4
Mongolian <abe546@live.com>
2021-02-21 16:41:36 -0500

    Time: 0 ms (100.00%), Space: 37.3 MB (49.77%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,92 +1,67 @@
 class Solution {
     private List<Integer> answer = new LinkedList(); 
     public List<Integer> spiralOrder(int[][] matrix) {
-        
-        int up = 0; 
-        int down = matrix.length - 1;
-        int right = matrix[0].length -1; 
-        int left = 0;
-        
-        int entry = 1; 
-        
-        while(entry <= matrix[0].length * matrix.length)
-        { 
-            //GO RIGHT
-            for(int i = up; i <= right; i++)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-           
-                
-                
-                answer.add(matrix[up][i]); 
-                entry++;
-            }
- 
-            up++;
-            //GO DOWN
-            for(int i = up; i <= down; i++)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-                
-                answer.add(matrix[i][right]); 
-                entry++;
-
-            }
-       
-            right--;
-            
-            //GO LEFT
-            for(int i = right; i >= left; i--)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-                
-                answer.add(matrix[down][i]);
-                entry++;
-
-            }
-            down--;
-            
-            //Go UP
-            for(int i = down; i >= up; i--)
-            {
-                if(entry > matrix[0].length * matrix.length)
-                {
-                    break;
-                }
-                
-                answer.add(matrix[i][left]);
-                entry++;
-            }
-            
-            left++;
-        }
   
+        int[][] clone = new int[matrix.length][matrix[0].length];
         
+        cloneMatrix(clone, matrix); 
+        
+        while(clone.length > 0)
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                for(int j = 0; j < clone[0].length; j++)
+                {
+                    answer.add(clone[i][j]); 
+                }
+            }
+            
+            clone = rotateMatrixMinus1(clone); 
+        }
+        
+        
+                
         return answer; 
-        
     }
     
-    public int[][] rotateArray90DegreesToTheLeft(int[][] matrix) {
-
-        int[][] tmpArray = new int[matrix[0].length][matrix.length-1];
-
-        for(int j=matrix[0].length -1; j>=0; j--)
+    public int[][] rotateMatrixMinus1(int[][] matrix)
+    {
+        int[][] rotate = new int[matrix[0].length][matrix.length-1];
+        
+        for(int col = 0; col < matrix[0].length; col++)
         {
-            for(int i=1; i<matrix.length; i++)
+            for(int row = 1; row < matrix.length; row++)
             {
-                tmpArray[ matrix[0].length - 1  - j][i-1] = matrix[i][j];
+                rotate[col][row-1] = matrix[row][matrix[0].length-1-col];
             }
         }
-
-        return tmpArray;
+        
+        return rotate; 
     }
+    
+    public void cloneMatrix(int[][] clone, int[][] matrix)
+    {
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix[0].length; j++)
+            {
+                clone[i][j] = matrix[i][j]; 
+            }
+        }
+    }
+    
+    public void printMatrix(int[][] matrix2)
+    {
+        for(int i = 0; i < matrix2.length; i++)
+        {
+            for(int j = 0; j < matrix2[0].length; j++)
+            {
+                System.out.print(" "+matrix2[i][j]); 
+            }
+            
+            System.out.println(""); 
+        }
+    }
+ 
+ 
 }
