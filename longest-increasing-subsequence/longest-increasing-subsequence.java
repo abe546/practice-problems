1694d6b2cdd11583caa3195a1892d9521afee0eb
Mongolian <abe546@live.com>
2021-02-22 17:25:13 -0500

    Time: 76 ms (22.75%), Space: 41.3 MB (16.04%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,32 +1,38 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-      
-      if(nums == null || nums.length == 0)
-      {
-        return 0; 
-      }
-      
-      int[] dp = new int[nums.length]; 
-      dp[0]=1;
-      
-      int max =1; 
  
-      for(int i=1; i<nums.length; i++)
-      { 
-        int count =0; 
-        for(int j=0; j<i;j++)
-        { 
-          if(nums[j]<nums[i])
-          {      
-            count = Math.max(count, dp[j]); 
-          }
+        if(nums == null || nums.length == 0)
+        {
+          return 0; 
         }
         
-        count++; 
-        dp[i] = count; 
-        max = Math.max(dp[i], max); 
-      }
-    
-      return max; 
+        int count = 0; 
+        int[] memory = new int[nums.length]; 
+        
+        memory[0] = 1;  
+        
+        for(int i = 1; i < nums.length; i++)
+        {
+            int myMemory = 0;
+            
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    myMemory = Math.max(memory[j], myMemory);
+                }
+            }
+            
+            memory[i] = myMemory + 1; //The 1 represents self 
+        }
+        
+        int max = 0; 
+        
+        for(int item : memory)
+        {
+            max = Math.max(item, max); 
+        }
+        
+        return max;
     }
 }
