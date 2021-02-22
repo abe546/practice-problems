849fae3a1e4da5f269b83d05fecbf2d3d4816db6
Mongolian <abe546@live.com>
2021-02-21 22:31:57 -0500

    Time: 0 ms (100.00%), Space: 39.6 MB (22.83%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -2,43 +2,44 @@ class Solution {
     List<List<Integer>> answer = new LinkedList(); 
     public List<List<Integer>> permute(int[] nums) {
         
-        int[] numsCopy = new int[nums.length];
+        int[] clone = new int[nums.length];
         
-        for(int i = 0; i<nums.length; i++)
+        for(int i = 0; i < nums.length; i++)
         {
-            numsCopy[i] = nums[i]; 
+            clone[i] = nums[i]; 
         }
         
-        permutations(numsCopy, 0 );
+        permutations(clone, 0); 
+        
         return answer;
     }
-    
+   
     public void permutations(int[] arr, int index)
     {
         for(int i = index; i < arr.length; i++)
         {
             int tmp = arr[i]; 
-            arr[i] = arr[index];
-            arr[index] = tmp; 
+            arr[i] = arr[index]; 
+            arr[index] = tmp;
             
             permutations(arr, index+1);
             
-            arr[index] = arr[i]; 
+            arr[index] = arr[i];            
             arr[i] = tmp; 
-            
         }
         
-        if(index == arr.length-1)
+        if(index == arr.length)
         {
             List<Integer> tmp = new LinkedList(); 
+            
             for(int i = 0; i < arr.length; i++)
             {
-                 tmp.add(arr[i]); 
+                tmp.add(arr[i]); 
             }
             
             answer.add(tmp); 
         }
     }
     
-   
-}
+    }
+ 
