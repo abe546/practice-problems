6f9aa706f9a3c2b2f6bb4da275c34156229fd46a
Mongolian <abe546@live.com>
2021-02-25 18:22:06 -0500

    Time: 0 ms (100.00%), Space: 36.3 MB (62.78%) - LeetHub

---
+++ house-robber/house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int[] rob = new int[nums.length]; 
+        
+        if(nums == null || nums.length == 0)
+        {
+            return 0; 
+        }
+        
+        if(nums.length == 1)
+        {
+            return nums[0]; 
+        }
+        
+        rob[0] = nums[0]; 
+        rob[1] = Math.max(nums[1], nums[0]);
+        
+        int stole = 0; 
+        
+        for(int i = 2; i < nums.length; i++)
+        {
+            rob[i] = Math.max(rob[i-1], rob[i-2]+nums[i]);
+        }
+        
+        return rob[rob.length-1]; 
+        
+    }
+}
