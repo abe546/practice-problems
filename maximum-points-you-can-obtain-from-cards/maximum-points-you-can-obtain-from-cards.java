e2712d40367e40fecc37f3374572b0cf3af99cdc
Mongolian <abe546@live.com>
2021-02-26 13:21:24 -0500

    Time: 242 ms (5.09%), Space: 47.5 MB (85.24%) - LeetHub

---
+++ maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+      
+        Deque<Integer> deque = new ArrayDeque(); 
+        
+        int[] extendedArr = new int[cardPoints.length + k]; 
+        int j = 0; 
+        for(int i = cardPoints.length; i < extendedArr.length; i++)
+        {
+            extendedArr[i] = cardPoints[j++];
+        }
+        
+        for(int i = 0; i < cardPoints.length; i++)
+        {
+            extendedArr[i] = cardPoints[i]; 
+        }
+        
+        int score = 0; 
+        int scoreRight = 0; 
+        for(int i = 0; i < k; i++)
+        {
+            score+= extendedArr[i]; 
+            deque.addLast(extendedArr[i]); 
+        }
+        
+        deque.clear(); 
+        
+        for(int i = cardPoints.length - k ; i < cardPoints.length; i++)
+        {
+            scoreRight+= extendedArr[i]; 
+            deque.addLast(extendedArr[i]); 
+        }
+        
+        
+        int tmpScore = scoreRight; 
+        for(int i = cardPoints.length; i < extendedArr.length; i++)
+        {
+            System.out.println("EXTENDED ARR [i] "+extendedArr[i]); 
+            
+            tmpScore = tmpScore - deque.getFirst();
+            tmpScore += extendedArr[i]; 
+            deque.removeFirst(); 
+            deque.addLast(extendedArr[i]); 
+            
+            score = Math.max(tmpScore, score); 
+        }
+ 
+        return Math.max(score, scoreRight);
+        
+    }
+    
+    public void printArr(int[] arr)
+    {
+        System.out.println(""); 
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(" "+arr[i]); 
+        }
+        
+        System.out.println(""); 
+    }
+}
