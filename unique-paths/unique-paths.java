c3ddf9cc53f4b139c7ed11adba65f93b1659362f
Mongolian <abe546@live.com>
2021-02-26 12:09:59 -0500

    Time: 0 ms (100.00%), Space: 35.6 MB (80.83%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -24,9 +24,7 @@ class Solution {
     for(int i = 1; i < m; i++)
     {
         for(int j = 1; j < n; j++)
-        {
- 
-            
+        {            
             if(matrix[i][j] == 0)
             {
                 matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
