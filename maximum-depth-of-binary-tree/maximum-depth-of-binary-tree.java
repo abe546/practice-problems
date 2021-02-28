89470cf5a77b658c68e9543d530e2130f0132cff
Mongolian <abe546@live.com>
2021-02-27 23:58:52 -0500

    Time: 0 ms (100.00%), Space: 40.2 MB (11.81%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -16,16 +16,65 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         
-        return findDepth(root, 0); 
+        return findMaxDepth(root, 0); 
     }
     
-    public int findDepth(TreeNode root, int level)
+    public int findMaxDepthIter(TreeNode root, int levelOrigin)
+    {
+        if(root == null)
+        {
+            return levelOrigin;
+        }
+        
+        int maxDepth = levelOrigin; 
+        
+        Map<TreeNode, Integer> nodeToLevel = new HashMap(); 
+        
+        nodeToLevel.put(root, levelOrigin); 
+        
+        Stack<Map<TreeNode, Integer>> nodes = new Stack(); 
+        
+        nodes.push(nodeToLevel); 
+        
+        while(!nodes.isEmpty())
+        {
+           Map<TreeNode, Integer> item = nodes.pop(); 
+           Map<TreeNode, Integer> child = null;  
+            
+            TreeNode source = (TreeNode)item.keySet().toArray()[0]; 
+            int level = item.get(source); 
+            level+=1; 
+            
+            maxDepth = Math.max(maxDepth, level); 
+            
+            if(source.left != null)
+            {
+                child = new HashMap(); 
+                child.put(source.left, level ); 
+                
+                nodes.push(child); 
+
+            }
+            
+            if(source.right != null)
+            {
+                child = new HashMap(); 
+                child.put(source.right, level ); 
+                
+                nodes.push(child); 
+            }
+        }
+        
+        return maxDepth; 
+    }
+    
+    public int findMaxDepth(TreeNode root, int level)
     {
         if(root == null)
         {
             return level;
         }
         
-        return Math.max(findDepth(root.left, level+1), findDepth(root.right, level+1)); 
+        return Math.max(findMaxDepth(root.left, level+1), findMaxDepth(root.right, level+1)); 
     }
 }
