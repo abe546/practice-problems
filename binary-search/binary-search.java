4255d7c0e86a9bdfcbc6a42fc514cae0dcb73db3
Mongolian <abe546@live.com>
2021-03-11 00:02:19 -0500

    Time: 0 ms (100.00%), Space: 40.1 MB (45.51%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
-        int index = -1;
-        int start = 0; 
-        int end = nums.length-1;
-        int mid = 0; 
-        while(start <= end){
-        mid = start + (end-start)/2;
-        
-            
-        if(target < nums[mid])
-        {
-            //look right of mid
-            end = mid-1;
-            
-           
-        }else if(target > nums[mid])
-        {
-            //look left of mid
-           
-            start = mid+1;
-        }else if(target == nums[mid]){
-            return mid; 
-        }
-        }
-        
-        return index; 
-        
-    }
+    public int search(int[] nums, int target) {
+        
+        int index = -1;
+        int start = 0; 
+        int end = nums.length-1;
+        int mid = 0; 
+        while(start <= end){
+        mid = start + (end-start)/2;
+        
+            
+        if(target < nums[mid])
+        {
+            //look right of mid
+            end = mid-1;
+            
+           
+        }else if(target > nums[mid])
+        {
+            //look left of mid
+           
+            start = mid+1;
+        }else if(target == nums[mid]){
+            return mid; 
+        }
+        }
+        
+        return index; 
+        
+    }
 }
