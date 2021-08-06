0113308483eb92b7f060162e56ed85dbfb9b1b49
Mongolian <abe546@live.com>
2021-08-06 02:55:34 -0400

    Time: 22 ms (37.60%), Space: 38.5 MB (82.60%) - LeetHub

--- number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,60 +1,52 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        //Use a sequence array to keep track of the longest sequence per iteration
-        //Use a count array to count the number of times an 'n' sequence occurs per iter
-        //e.g. theres two subsequences that occur of equal size, 
-        //count for seq[i] will then be 2
-        //By default seq array counts set to 1
-        //By default count array counts set to 1
-        //Note count[i] represents the max count per iteration
-        
-        if(nums == null && nums.length == 0)
+      
+        if(nums == null || nums.length == 0)
         {
-            return 0;
+            return 0; 
         }
         
-        int[] seq = new int[nums.length];
+        int max = 1; 
+        int[] frequency = new int[nums.length];
         int[] count = new int[nums.length];
         
-        //Default count is 1 and default seq length is 1
-        Arrays.fill(seq, 1);
-        Arrays.fill(count, 1);
-        
-        int maxSeq = 1; 
+        Arrays.fill(frequency, 1); 
+        Arrays.fill(count, 1); 
         
         for(int i = 0; i < nums.length; i++)
         {
             for(int j = 0; j < i; j++)
             {
+                
+                
                 if(nums[j] < nums[i])
                 {
-                    if(seq[j] == seq[i]-1)
+                    if(frequency[i] - 1 == frequency[j])
                     {
-                        count[i] = count[j] + count[i];
-                    }else if(seq[j] >= seq[i])
-                    {
-                        seq[i] = seq[j]+1;
-                        count[i] = count[j];
-                        
-                        maxSeq = Math.max(maxSeq, seq[i]); 
-                        
-                    }
+                    count[i] += count[j]; 
+                    continue;
+                    }else if( frequency[j] >= frequency[i]){
+                    
+                    frequency[i] = Math.max(frequency[i], frequency[j] + 1);
+                    count[i] = count[j]; 
+                    max = Math.max(frequency[i], max);                    
                 }
             }
         }
+        }
         
         int answer = 0; 
-        
-        for(int i = 0; i < seq.length; i++)
+        for(int i = 0; i < count.length; i++)
         {
-            int item = seq[i];
+            int item = frequency[i]; 
             
-            if(item == maxSeq)
+            if(item == max)
             {
-                answer += count[i];
+                answer+=count[i]; 
             }
         }
         
-        return answer; 
+        return answer;
+        
     }
 }
