afad8b23d5ed1de7639dffafd41c07db448f63e0
Mongolian <abe546@live.com>
2021-09-20 02:16:20 -0400

    Time: 0 ms (100.00%), Space: 37.7 MB (19.67%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,39 +1,33 @@
-import java.awt.*;
-
 class Solution {
-    
-    public int uniquePaths(int m, int n)
-    {
-         if(m == 0 && n == 0)
+    public int uniquePaths(int m, int n) {
+        
+        if(n == 0 || m == 0)
         {
             return 0;
         }
- 
-        int[][] matrix = new int[m][n]; 
         
-        for(int i = 0; i < m; i++)
-        {
-            matrix[i][0] = 1; 
-        }
-        
-        for(int j= 0; j < n; j++)
-        {
-            matrix[0][j] = 1;
-        }
-        
-        for(int i = 1; i < m; i ++)
-        {
-            for(int j = 1; j < n; j++)
+        int[][] arr = new int[m][n];
+     
+        for(int j = 0; j < n; j++)
             {
-                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                arr[0][j] = 1;
+            }    
+       
+       
+        
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                arr[i][j] += arr[i-1][j];
+                
+                if(j > 0){
+                    arr[i][j] += arr[i][j-1];
+                }
             }
         }
         
+        return arr[m-1][n-1];
         
-        return matrix[m-1][n-1]; 
- 
-     
     }
-    
-     
 }
