f784564d7dc3a32cb04b6ef216391059149b80b7
Mongolian <abe546@live.com>
2021-11-06 04:11:43 -0400

    Time: 99 ms (10.98%), Space: 39.3 MB (59.45%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,20 +1,19 @@
 class Solution {
+        
     public int[] twoSum(int[] nums, int target) {
-
+    
         for( int i = 0; i < nums.length; i++)
         {
-            for( int j = 0; j < nums.length; j++)
+            for(int j = 0; j < nums.length; j++)
             {
-                if( i == j)
+                if(i == j)
                 {
                     continue; 
                 }
                 
-                if(target == nums[i] + nums[j])
+                if(nums[i] + nums[j] == target)
                 {
-                    int arr[] = {i, j};
-                    
-                    return arr;
+                    return new int[] {i, j};
                 }
             }
         }
