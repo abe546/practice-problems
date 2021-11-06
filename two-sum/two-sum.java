341225728ed826bb28626485fb013c144acf96b4
Mongolian <abe546@live.com>
2021-11-06 02:42:41 -0400

    Time: 99 ms (10.98%), Space: 39.2 MB (66.57%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,28 +1,26 @@
 class Solution {
+     
     public int[] twoSum(int[] nums, int target) {
-        
-        Map<Integer, Integer> map = new HashMap();
-        
-        for(int i = 0; i < nums.length; i++)
+ 
+        for( int i = 0; i < nums.length; i++)
         {
-            int item = nums[i];
-            
-            map.put(item, i);
-        }
-        
-        for(int i =0 ; i < nums.length; i++)
-        {
-            int item = nums[i];
-            
-            if(map.get(target - item) != null && map.get(target - item) != i)
+            for(int j = 0; j < nums.length; j++)
             {
-                int[] arr = new int[2];
-                arr[0] = i; 
-                arr[1] = map.get(target-item);
-                return arr;
+                if(i == j)
+                {
+                    continue; 
+                }
+                
+                if(nums[i] + nums[j] == target)
+                {
+                    int arr[] = {i,j};
+                    
+                    return arr;
+                }
             }
         }
         
-        return null; 
+        return null;
+        
     }
 }
