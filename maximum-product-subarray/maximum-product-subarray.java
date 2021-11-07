44d96dd1eb9be60d4c097d2f23d68c43a3772d44
Mongolian <abe546@live.com>
2021-11-07 04:11:11 -0500

    Time: 128 ms (5.01%), Space: 38.9 MB (64.65%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxProduct(int[] nums) {
-    
+      
+        int sum = nums[0]; 
         int max = nums[0];
-        int sum = 0; 
+        
         for( int i = 0; i < nums.length; i++)
         {
             sum = nums[i]; 
-            max = Math.max(sum, max);
+
             for( int j = i+1; j < nums.length; j++)
             {
                 sum *= nums[j]; 
-                
-                max = Math.max(sum, max);
+                max = Math.max(sum, Math.max(nums[j], max));
             }
         }
         
