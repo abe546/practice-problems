9664bd3ebe3880664a8077885fb1b5d8c2be28cc
Mongolian <abe546@live.com>
2021-11-11 03:19:13 -0500

    Time: 1 ms (99.13%), Space: 38.8 MB (63.64%) - LeetHub

--- reverse-bits/reverse-bits.java
+++ reverse-bits/reverse-bits.java
@@ -3,21 +3,23 @@ import java.math.BigInteger;
 public class Solution {
     // you need treat n as an unsigned value
      public int reverseBits(int n) {
-        int temp=0;
-         System.out.println("N : "+Integer.toBinaryString(n));
-        for(int i=0;i<32;i++){
-            int val=n&1;
-            System.out.println("VAL : "+Integer.toBinaryString(val));
-             System.out.println("TEMP 0 : "+Integer.toBinaryString(temp));
-            temp=temp<<1;
-            System.out.println("TEMP 1 : "+Integer.toBinaryString(temp));
-            temp+=val; 
-            System.out.println("TEMP 2 : "+Integer.toBinaryString(temp));
+        
+         //Add the last element of n as the first element of some new item
+         //n times
+         
+         int mask = n; 
+         int item = 0; 
+         
+         for(int i = 0; i < 32; i++)
+         {
+             int value = mask & 1; 
+             mask = mask >> 1; 
+             item = item << 1; 
+             item ^= value; 
             
-            n=n>>>1;
-            System.out.println("N : "+Integer.toBinaryString(n));
-
-        }
-        return temp;
+             
+         }
+         
+         return item; 
     }
 }
