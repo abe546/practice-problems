a7d672f60b69acf28638cb459464bc0ed42ca374
Mongolian <abe546@live.com>
2021-11-28 02:33:28 -0500

    Time: 13 ms (67.34%), Space: 38 MB (97.89%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,42 +1,41 @@
 class Solution {
  
-    
     public int coinChange(int[] coins, int amount) {
+    
+        int[] answer = new int[amount+1];
         
-    int[] answer = new int[amount+1];
+        Arrays.fill(answer, Integer.MAX_VALUE-1);
+        Arrays.sort(coins);
         
-    Arrays.sort(coins);
-  
-    Arrays.fill(answer, amount+1); 
-    answer[0] = 0; 
         
-    for(int i = 1; i <= amount; i++)
-    {
-        for(int j = 0; j < coins.length; j++)
+        answer[0] = 0; 
+        
+        for( int i = 1; i <= amount; i++)
         {
-            
-            int coin = coins[j]; 
-         
-            if(coin > i)
+            for(int j = 0; j < coins.length; j++)
             {
-                break;
+                int coinAmount = i;
+                int smallerCoin = coins[j];
+                
+                if(coins[j] > coinAmount)
+                {
+                    break; 
+                }
+                
+                if(smallerCoin <= coinAmount)
+                {
+                    answer[coinAmount] = 
+                        Math.min(answer[coinAmount], answer[coinAmount - smallerCoin]+1); 
+                }
+                
             }
-            
-           if(coin <= i)
-           {
-               answer[i] = Math.min(answer[i], answer[i-coin]+1);
-           }
         }
-    }
- 
+        
         if(answer[amount] > amount)
         {
-            return -1; 
+            return -1;
         }
         
-        System.out.println("AMOUNT : "+amount); 
-        System.out.println("ANSWER AMOUNT : "+answer[amount]); 
-        
-     return answer[amount];    
+        return answer[amount]; 
     }
 }
