65892d7103ddc21ccdd23eac59fa575c33095f64
Mongolian <abe546@live.com>
2021-11-29 09:58:35 -0500

    Time: 0 ms (100.00%), Space: 36.5 MB (60.29%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -12,18 +12,27 @@ class Solution {
         {
             return nums[0]; 
         }
+     
+        
+        return findHousesToRob(nums); 
+        
+    }
+    
+    public int findHousesToRob(int[] nums)
+    { 
+        int[] rob = new int[nums.length];
         
         rob[0] = nums[0]; 
         rob[1] = Math.max(nums[1], nums[0]);
-        
-        int stole = 0; 
+ 
         
         for(int i = 2; i < nums.length; i++)
-        {
+        { 
+             
+    
             rob[i] = Math.max(rob[i-1], rob[i-2]+nums[i]);
         }
         
         return rob[rob.length-1]; 
-        
     }
 }
