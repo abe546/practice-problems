73418a8b4fd26ab60fd84ba77f95f6be4345204e
Mongolian <abe546@live.com>
2021-12-02 02:05:40 -0500

    Time: 0 ms (100.00%), Space: 36.3 MB (73.85%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,38 +1,37 @@
 class Solution {
     public int rob(int[] nums) {
-        
-        int[] rob = new int[nums.length]; 
-        
-        if(nums == null || nums.length == 0)
-        {
-            return 0; 
-        }
+  
         
         if(nums.length == 1)
         {
-            return nums[0]; 
+            return nums[0];
         }
-     
         
-        return findHousesToRob(nums); 
+        if(nums.length == 2)
+        {
+            return Math.max(nums[0], nums[1]);
+        }
+        
+       int[] arr = new int[nums.length];
+        
+        arr[0] = nums[0]; 
+        arr[1] = nums[1]; 
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i == 0){
+            arr[i] = nums[i];
+            }else if(i == 1)
+            {
+                arr[i] = Math.max(nums[i], arr[i-1]); 
+                
+            }else{
+            arr[i] = Math.max(nums[i] + arr[i-2], arr[i-1]);
+            }
+        }
+        
+        return arr[arr.length-1];
         
     }
-    
-    public int findHousesToRob(int[] nums)
-    { 
-        int[] rob = new int[nums.length];
-        
-        rob[0] = nums[0]; 
-        rob[1] = Math.max(nums[1], nums[0]);
  
-        
-        for(int i = 2; i < nums.length; i++)
-        { 
-             
-    
-            rob[i] = Math.max(rob[i-1], rob[i-2]+nums[i]);
-        }
-        
-        return rob[rob.length-1]; 
-    }
 }
