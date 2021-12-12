96ac482d07d54baf3acbb14749e2cfda7cacc01d
Mongolian <abe546@live.com>
2021-12-11 19:11:34 -0500

    Time: 8 ms (7.14%), Space: 38.9 MB (29.84%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,59 +1,55 @@
 class Solution {
- 
     Map<String, String> map = new HashMap();
     Map<Integer, Integer> memo = new HashMap(); 
     public int numDecodings(String s) {
- 
-        String alphabet = "ABCDEFGHIJKLMNOPGRSTUVWXYZ";
+          String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         
-        int count = 1;
+        int count = 1; 
         for(char item : alphabet.toCharArray())
         {
-            map.put(String.valueOf(count++), ""+item);
+            map.put(String.valueOf(count++), String.valueOf(item));
         }
         
-        return getDecodeCount(s, 0);
+        return decode(s, 0);
     }
     
-    public int getDecodeCount(String input, int index)
-    {     
+    public int decode(String input, int index)
+    {
         if(memo.get(index) != null)
         {
-            return memo.get(index);
-        }      
+            return memo.get(index); 
+        }
+        
         String me = null; 
         
-        if(index < input.length()){
-        me = input.charAt(index)+"";
-        
-        if(map.get(me) == null)
+        if(index < input.length())
         {
+            me = String.valueOf(input.charAt(index));
+            
+            if(map.get(me) == null)
+            {
+                return 0;
+            }
+        }else{
+            return 1;
+        }
         
-            return 0; 
-        }
-        }
-         
-        if(input == null || input.length() == 0 || index >= input.length() -1)
+        int count = 0; 
+        
+        count+= decode(input, index+1);
+        
+        if(index+1 <input.length())
         {
-            return 1; 
-        } 
-        
-        
-        
-        int count = 0;
-        count +=  getDecodeCount(input, index+1);
-        
-        if(input.length() >= index+1){
-        me = me + input.charAt(index+1); 
-         
-        if(map.get(me) != null)
-        {
-            count += getDecodeCount(input, index+2);
-        }
+            me = me + input.charAt(index+1);
+            
+            if(map.get(me) != null)
+            {
+             count += decode(input, index+2); 
+            }
         }
         
         memo.put(index, count); 
         
         return count; 
-    }   
+    }
 }
