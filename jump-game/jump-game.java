35057e1eab06003dded79bf3298a0f2ae1ee8558
Mongolian <abe546@live.com>
2021-12-13 01:03:03 -0500

    Time: 12 ms (33.87%), Space: 40.6 MB (34.33%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,25 +1,51 @@
 class Solution {
+    
+    Map<Integer, Integer> map = new HashMap(); 
+    
     public boolean canJump(int[] nums) {
- 
-    int index = nums[0];    
-        
-    for(int i = 0; i <= index; i++)
-    {
-        if(index >= nums.length -1)
+      
+        if(nums.length == 1)
         {
-            return true; 
+            return true;
         }
         
-        if(i+nums[i] > index)
-        {
-            index = i+nums[i]; 
-        }else
-        {
-            index = Math.max(index, i); 
+        int limit = nums[0]; 
+        int max = nums[0]+1;
+        map.put(max, 0); 
+        int i = 1; 
+        int lastIndex = 0; 
+        while(i <= limit){
+            
+            if(limit >= nums.length-1)
+            {
+                return true; 
+            }
+            
+            if(max <= nums[i]+(i))
+            {
+                max = nums[i]+i;
+                map.put(max, i);
+            }
+             
+            if(i == limit)
+            {
+                limit = max;
+                i = map.get(max);
+                max = 0; 
+                
+                if(lastIndex == i)
+                {
+                    return false; 
+                }
+                
+                lastIndex = i; 
+            }
+            
+            i++;
         }
-    }
- 
-        return false;
+  
         
+         
+        return false; 
     }
 }
