1c5f299d4abbd8f09ca70f1afae4113abad2a17f
Mongolian <abe546@live.com>
2021-12-31 04:05:11 -0500

    Time: 70 ms (5.12%), Space: 49.6 MB (19.17%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,90 +1,90 @@
 class Solution {
-    private boolean[][] ignore;
-   //Left, right, up, down 
-   private int[][] dir = {
-      {1,0},
-      {-1,0},
-      {0,1},
-      {0,-1}
+    //Array of coordinates
+    //Where index 1 is row, index 2 is column
+    int[][] boundary = {
+        {0,1},
+        {1,0},
+        {0,-1},
+        {-1,0}
     };
+    
+    //List<Integer> represents coordinates in a matrix
+    //Where the first entry in the list is row, and second entry in the list is column
+    // E.g. {1, 2} -> row 1 column 2
+    Queue<List<Integer>> oneCoordinates = new ArrayDeque();
+    Set<List<Integer>> explored = new HashSet();
+    Set<List<Integer>> avoid = new HashSet(); 
     public int numIslands(char[][] grid) {
-      boolean contains1 = false; 
-      for(int i=0; i<grid.length; i++)
-      {
-        for(int j=0; j<grid[0].length; j++)
-        {
-          if(grid[i][j] == '1')
-          {
-            contains1 = true; 
-            break; 
-          }
-        }
-      }
-      
-      if(!contains1)
-      {
-        return 0; 
-      }
-      int count =0; 
-      ignore = new boolean[grid.length][grid[0].length]; 
-      
-     
-      
-      for(int i=0; i<grid.length; i++)
-      {
-        for(int j=0; j<grid[0].length; j++)
-        {
-          if(grid[i][j] == '1' && ignore[i][j] == false)
-          {
-              Stack<List<Integer>> stack = new Stack();
-             List<Integer> tmp = new ArrayList(); 
-            tmp.add(i); tmp.add(j); 
-            stack.push(tmp);
-           
-            
-            while(!stack.isEmpty())
-            {
-                List<Integer> list = stack.pop();
-                
-              int x=list.get(0); 
-              int y = list.get(1); 
-              
-              ignore[x][y] = true; 
-              
-              explore(x, y, grid, stack, ignore); 
-           
-            }
-            count++; 
-          }
         
-        }
-      }
-      
-      return count; 
-    }
-  
-  public void explore(int i, int j, char[][] grid, Stack<List<Integer>> stack, boolean[][] ignore)
-  {
-    
-    
-    for(int[] move : dir)
+    for(int i = 0; i < grid.length; i++)
     {
-      int x = i + move[0]; 
-      int y = j+ move[1]; 
-      
-      if(x>=0 && x<grid.length && y>=0 && y<grid[0].length && ignore[x][y] == false)
-      {
-        if(grid[x][y] == '1')
+        for(int j = 0; j < grid[0].length; j++)
         {
-          List<Integer> tmp = new ArrayList(); 
-          tmp.add(x); tmp.add(y); 
-          stack.push(tmp);
-          
-          ignore[x][y] = true; 
-          
+            if(grid[i][j] == '1')
+            {
+                List<Integer> tmp = new LinkedList(); 
+                tmp.add(i); 
+                tmp.add(j); 
+                oneCoordinates.add(tmp);
+            }  
         }
-      }
+    }
+        
+    int islands = 0;     
+    while(!oneCoordinates.isEmpty())
+    {
+        List<Integer> current = oneCoordinates.poll(); 
+        avoid.clear(); 
+        
+        if(!explored.contains(current)){
+        explore(grid, current, explored);
+        islands++; 
+        }
+    }
+        
+        return islands;
     }
     
-  }
+    public void explore(char[][] matrix, List<Integer> coordinate, Set<List<Integer>> explored)
+    {
+        int i = coordinate.get(0); 
+        int j = coordinate.get(1); 
+        
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length)
+        {
+            return;
+        }
+        
+        if(matrix[i][j] == '0')
+        {
+            return; 
+        }
+        
+        if(avoid.contains(coordinate))
+        {
+            return;
+        }
+        
+        if(explored.contains(coordinate))
+        {
+            return;
+        }
+        
+        avoid.add(coordinate);
+        explored.add(coordinate); 
+        
+        for(int[] dir : boundary)
+        {
+            int x = i + dir[0]; 
+            int y = j + dir[1];
+            
+            List<Integer> tmp = new LinkedList(); 
+            
+            tmp.add(x); 
+            tmp.add(y); 
+            
+            explore(matrix, tmp, explored);
+        }
+        
+    }
 }
