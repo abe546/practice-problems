a88738051411cc8c6c57050c8caf97bbed864559
Mongolian <abe546@live.com>
2022-01-23 05:57:36 -0500

    Time: 117 ms (17.37%), Space: 110.3 MB (32.07%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
   class myComparator implements Comparator<int[]> {
     public int compare(int[] a, int[] b) {
-      return a[1] - b[1];
+      return a[0] - b[0];
     }
   }
 
@@ -10,14 +10,17 @@ class Solution {
       return 0;
     }
     Arrays.sort(intervals, new myComparator());
-    int end = intervals[0][1];
-    int count = 1;
+    int   prev = 0, count = 0;
     for (int i = 1; i < intervals.length; i++) {
-      if (intervals[i][0] >= end) {
-        end = intervals[i][1];
+      if (intervals[prev][1] > intervals[i][0]) {
+        if (intervals[prev][1] > intervals[i][1]) {
+          prev = i;
+        }
         count++;
+      } else {
+        prev = i;
       }
     }
-    return intervals.length - count;
+    return count;
   }
 }
