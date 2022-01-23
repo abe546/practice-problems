d4c19ec7ea4a22b5b0c489f1c7230a91007635d0
Mongolian <abe546@live.com>
2022-01-23 05:33:53 -0500

    Time: 88 ms (39.32%), Space: 108.8 MB (40.99%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,58 +1,23 @@
 class Solution {
-    public int eraseOverlapIntervals(int[][] intervals) {
-     
-        
-        int[][] copy = new int[intervals.length][intervals[0].length];
-        for(int i = 0; i < copy.length; i++)
-        {
-            copy[i][0] = intervals[i][0];
-            copy[i][1] = intervals[i][1];
+  class myComparator implements Comparator<int[]> {
+    public int compare(int[] a, int[] b) {
+      return a[1] - b[1];
+    }
+  }
 
-        }
-        
-        sortArrOfArrs(copy);
-        
-        int current = 0; 
-        int size =1; 
-        while(current < copy.length)
-        {
-            int[] curr = copy[current];
-  
-            int end = curr[1];
-            current++;
-            
-            while(current < copy.length)
-            {
-                int[] next = copy[current];
-                    
-                if(next[0] >= end){
- 
-                    end = next[1];
-            
-                    size++;
-                } 
-                
-                    current++;
-            }
-        }
-        
-        return intervals.length - size;
+  public int eraseOverlapIntervals(int[][] intervals) {
+    if (intervals.length == 0) {
+      return 0;
     }
-    
-    public void sortArrOfArrs(int[][] arr)
-    {
-        Arrays.sort(arr,
-                   (int[] a, int[] b) ->
-                    {
-                       if(a[1] < b[1])
-                       {
-                           return -1;
-                       }else if(a[1] > b[1])
-                       {
-                           return 1;
-                       }
-                        
-                        return 0; 
-                    });
+    Arrays.sort(intervals, new myComparator());
+    int end = intervals[0][1];
+    int count = 1;
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] >= end) {
+        end = intervals[i][1];
+        count++;
+      }
     }
+    return intervals.length - count;
+  }
 }
