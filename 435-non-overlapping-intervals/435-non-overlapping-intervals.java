62e2072a1fcd512927ab7dcecd5ee878a2142ef1
Mongolian <abe546@live.com>
2022-01-23 22:48:25 -0500

    Time: 124 ms (12.58%), Space: 108.1 MB (42.17%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,46 +1,60 @@
 class Solution {
 
   public int eraseOverlapIntervals(int[][] intervals) {
-  
-      //In an interview, ask if its ok, for the sake of item, to alter input
+      
+      //Is it alright to alter input for sake of time?
       sortArrayOfArrays(intervals);
       
-      int start = intervals[0][0];
-      int end = intervals[0][1];
-      int count = 0; 
-      int index = 0;
-      int iter =  1;
-     
-      for(int i = 1; i < intervals.length; i++)
+      int end = intervals[0][1]; 
+      int start = intervals[0][0]; 
+      int index = 0; 
+      int iter = 1; 
+      int remove = 0;
+      while(index < intervals.length && iter < intervals.length)
       {
-           int[] next = intervals[i];
+          int[] next = intervals[iter];
           
           if(end > next[0])
           {
-              if(next[1] < end)
+              
+              
+              if(end > next[1])
               {
-                  index = i;
-                  end = intervals[index][1];
+                  index = iter; 
+                  end = next[1];
+                 
               }
-              count++;
+              iter++;
+               remove++;
+    
+          
           }else
           {
-              index = i; 
+           
+              index = iter; 
               end = intervals[index][1];
+    iter++;
           }
       }
-      
-      return count; 
-      
+   
+      return remove;
   }
     
     
     public void sortArrayOfArrays(int[][] intervals)
-    {
+    { 
         Arrays.sort(intervals,
-                   (int[] a, int[] b)
-                   ->{
-                      return a[0] - b[0]; 
+                    (int[] a, int[] b)
+                   -> {
+                       if(a[0] < b[0])
+                       {
+                           return -1;
+                       }else if(b[0] < a[0])
+                       {
+                           return 1;
+                       }
+                       
+                       return 0;
                    });
     }
    
