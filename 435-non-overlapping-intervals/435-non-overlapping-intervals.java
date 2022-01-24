a5e36734a688897cd9839a2e6084ec23e5f7a5c4
Mongolian <abe546@live.com>
2022-01-23 23:03:43 -0500

    Time: 48 ms (70.12%), Space: 96.7 MB (56.80%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -2,60 +2,53 @@ class Solution {
 
   public int eraseOverlapIntervals(int[][] intervals) {
       
-      //Is it alright to alter input for sake of time?
+      //Is it ok, for the sake of time in interview, to sort the input array?
       sortArrayOfArrays(intervals);
       
       int end = intervals[0][1]; 
-      int start = intervals[0][0]; 
-      int index = 0; 
-      int iter = 1; 
+      int start = intervals[0][0];
       int remove = 0;
+      int index = 0; 
+      int iter = 1;
+      
       while(index < intervals.length && iter < intervals.length)
       {
           int[] next = intervals[iter];
           
           if(end > next[0])
           {
-              
+              remove++; 
               
               if(end > next[1])
               {
-                  index = iter; 
-                  end = next[1];
-                 
+                  end = next[1]; 
               }
-              iter++;
-               remove++;
-    
-          
           }else
           {
-           
-              index = iter; 
-              end = intervals[index][1];
-    iter++;
+              end = next[1]; 
           }
+          
+          iter++; 
       }
-   
-      return remove;
+      
+      return remove; 
+      
   }
     
-    
-    public void sortArrayOfArrays(int[][] intervals)
-    { 
-        Arrays.sort(intervals,
-                    (int[] a, int[] b)
-                   -> {
-                       if(a[0] < b[0])
-                       {
-                           return -1;
-                       }else if(b[0] < a[0])
-                       {
-                           return 1;
-                       }
-                       
-                       return 0;
-                   });
-    }
+  public void sortArrayOfArrays(int[][] intervals)
+  {
+      Arrays.sort(intervals, (int[] a, int[] b)
+                 -> {
+                    if(a[0] < b[0])
+                    {
+                        return -1;
+                    }else if(b[0] < a[0])
+                    {
+                        return 1; 
+                    }
+                     
+                     return 0; 
+                 });
+  }
    
 }
