a2dc7ded0e41b8b4dde4f04a02fdb7ba526c069d
Mongolian <abe546@live.com>
2022-01-30 23:45:10 -0500

    Time: 258 ms (21.17%), Space: 42 MB (15.90%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,66 +1,82 @@
 class Solution {
+    private static final int[][] directions =
+            {
+                    {1, 0},
+                    {0, 1},
+                    {-1, 0},
+                    {0, -1}
+            };
+    private boolean[][] ignore;
 
-    private int[][] boundary = {
-        {0,1},
-        {1,0},
-        {-1,0},
-        {0,-1}
-    };
-    private int[][] ignore = null; 
     public boolean exist(char[][] board, String word) {
- 
-        ignore = new int[board.length][board[0].length]; 
-        
-     for(int i = 0; i < board.length; i++)
-     {
-         for(int j = 0; j < board[0].length; j++)
-         {
-             ignore = new int[board.length][board[0].length]; 
-             if(explore(i, j, board, word, 0))
-             {
-                 return true; 
-             }
-         }
-     }
-        
-        return false;
-    }
-    
-    public boolean explore(int i, int j, char[][] matrix, String word, int index)
-    {
-            
-        if(ignore[i][j] == 1)
-        {
+
+        if (word == null || word.length() == 0 && board.length != 0) {
             return false;
         }
-        
-        if(word.charAt(index) == matrix[i][j])
-        {
-            if(index+1 >= word.length())
-            {
-                return true; 
-            }
-            
-            ignore[i][j] = 1;
-            
-            for(int[] dir : boundary)
-            {
-                int x = dir[0] + i;
-                int y = dir[1] + j;
-                
-                if(x < matrix.length && y < matrix[0].length && x >= 0 && y >= 0 && ignore[x][y] != 1)
-                {
-                    if(explore(x, y, matrix, word, index+1))
-                    {
-                        return true; 
-                    }
+
+        ignore = new boolean[board.length][board[0].length];
+
+        Stack<List<Integer>> examine = new Stack();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    List<Integer> tmp = new LinkedList();
+                    tmp.add(i);
+                    tmp.add(j);
+                    examine.push(tmp);
                 }
             }
-            
-            ignore[i][j] = 0; 
         }
-        
-        return false; 
+
+        while (!examine.isEmpty()) {
+
+            List<Integer> entry = examine.pop();
+            int i = entry.get(0);
+            int j = entry.get(1);
+
+            if (explore(i, j, board, 0 + 1, word)) {
+                return true;
+            }
+
+
+        }
+
+
+        return false;
     }
-     
+
+    public boolean explore(int i, int j, char[][] board, int charIndex, String word) {
+
+        if (charIndex >= word.length()) {
+            return true;
+        }
+
+        if (ignore[i][j] == true) {
+            return false;
+        }
+
+        //We will ignore any source/parent cells
+        ignore[i][j] = true;
+
+        boolean statement = false;
+        for (int[] dir : directions) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+
+
+            if (x >= 0 && y >= 0 && y < board[0].length && x < board.length && board[x][y] == word.charAt(charIndex) && ignore[x][y] == false) {
+                if (!statement) {
+                    statement = explore(x, y, board, charIndex + 1, word);
+                }
+
+            }
+        }
+
+        //Undo ignore, as we should be able to examine this field again in the futre checks
+        ignore[i][j] = false;
+
+        return statement;
+
+    }
 }
