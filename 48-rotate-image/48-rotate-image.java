f352141929e5605f9150d009529fb6773cf0006c
Mongolian <abe546@live.com>
2022-01-30 22:44:54 -0500

    Time: 0 ms (100.00%), Space: 43.1 MB (5.10%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,22 +1,50 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        List<Integer> items = new ArrayList(); 
-        
+      
+        //Transpose matrix
+        transpose(matrix);
+
+        //Reflext matrix
+        reflect(matrix); 
+ 
+    }
+    
+    public void transpose(int[][] matrix)
+    {
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = i + 1; j < matrix[0].length; j++)
+            {
+                int tmp = matrix[i][j]; 
+                
+                matrix[i][j] = matrix[j][i]; 
+                matrix[j][i] = tmp; 
+            }
+        }
+    }
+    
+    public void reflect(int[][] matrix)
+    {
+        for(int j = 0; j < matrix[0].length/2; j++)
+        {
+            for(int i = 0; i < matrix.length; i++)
+            {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length-1-j];
+                matrix[i][matrix[0].length-1-j] = tmp; 
+            }
+        }
+    }
+    
+    public void printArr(int[][] matrix)
+    {
         for(int i = 0; i < matrix.length; i++)
         {
             for(int j = 0; j < matrix[0].length; j++)
             {
-                items.add(matrix[i][j]);
+                System.out.print(" "+matrix[i][j]+" ");
             }
-        }
-        
-        int k = 0; 
-        for(int j = matrix[0].length-1; j >= 0; j--){
-            
-        for(int i = 0; i < matrix.length; i++)
-        {
-         matrix[i][j] = items.get(k++);   
-        }
+             System.out.println();
         }
     }
 }
