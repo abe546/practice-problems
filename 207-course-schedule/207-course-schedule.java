96b37a4cdfd5add8b0330e6bca696c39313d2acb
Mongolian <abe546@live.com>
2022-02-10 04:11:20 -0500

    Time: 39 ms (9.71%), Space: 53.4 MB (6.44%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,76 @@
+class Solution {
+    
+    private     Map<Integer, Boolean> map = new HashMap(); 
+    private List<Integer> list = new LinkedList(); 
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+     
+    Map<Integer, Set<Integer>> graph = new HashMap();  
+    Set<Integer> memory = new HashSet<Integer>();
+        
+
+        
+    createGraph(graph, prerequisites);
+        
+        for(int[] items : prerequisites)
+        {
+            list.clear(); 
+            if(!allowed(items[0], memory, graph)){
+                return false; 
+            }
+            
+            System.out.println("LIST : "+list); 
+        }
+    
+    return true;     
+    }
+    
+    public boolean allowed(int node, Set<Integer> memory, Map<Integer, Set<Integer>> graph)
+    {
+        if(map.get(node) != null)
+        {
+            return map.get(node); 
+        }
+        
+        if(memory.contains(node))
+        {
+            return false; 
+        }
+        
+        if(graph.get(node) == null)
+        {
+            list.add(node);
+            //no children
+            return true; 
+        }
+        
+        memory.add(node); 
+        
+        for(int child : graph.get(node))
+        {
+            if(!allowed(child, memory, graph))
+            {
+                return false; 
+            }
+        }
+        list.add(node); 
+        memory.remove(node); 
+        map.put(node, true);
+        
+        return true; 
+    }
+    
+    public void createGraph(Map<Integer, Set<Integer>> graph, int[][] prereqs)
+    {
+        for(int i = 0; i < prereqs.length; i++)
+        {
+            //Given preres[0-n].length == 2
+            int from = prereqs[i][0];
+            int to = prereqs[i][1]; 
+            
+            Set<Integer> tmp = graph.getOrDefault(from, new HashSet<Integer>());
+            tmp.add(to); 
+            
+            graph.put(from, tmp); 
+        }
+    }
+}
