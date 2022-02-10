c90ac2b7e5c26bc8220de2c361ad0d1d0e67608c
Mongolian <abe546@live.com>
2022-02-10 01:46:09 -0500

    Time: 255 ms (5.65%), Space: 72.6 MB (5.03%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,14 +1,11 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
   
-   
-        
     ArrayList<Integer> frequency = new ArrayList();
     
-        for(int i = 0; i < nums.length; i++)
-        { 
+        for(int item : nums)
+        {
             frequency.add(1);
-         
         }
         
     int max = 1; 
