352cd150a7f777172ec5598ac7f568811b2b8441
Mongolian <abe546@live.com>
2022-02-13 23:39:30 -0500

    Time: 810 ms (5.09%), Space: 145.6 MB (5.01%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -0,0 +1,69 @@
+class Trie {
+
+    private Map<Character, Trie> map; 
+    private boolean endOfWord = false; 
+    
+    public Trie() {
+ map = new HashMap(); 
+    }
+    
+    public void insert(String word) {
+        Trie curr = this;
+        
+        for(int i=0; i<word.toCharArray().length; i++)
+        {             
+            if(curr.map.get(word.charAt(i)) == null)
+            {
+                curr.map.put(word.charAt(i), new Trie());
+            }
+            if(i<word.toCharArray().length){
+            curr = curr.map.get(word.charAt(i));
+            }
+        }
+        
+        curr.endOfWord = true; //Stamp the last char of this word for examination later
+    }
+    
+    public boolean search(String word) {
+        
+        Trie current = this;
+        
+        for(char item : word.toCharArray())
+        {
+            if(current.map.get(item) == null)
+            {
+                return false; 
+            }
+            
+            current = current.map.get(item);
+        }
+        
+        return current.endOfWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        
+        Trie current = this;
+    
+        for(char item : prefix.toCharArray())
+        {
+            if(current.map.get(item) == null)
+            {
+                return false; 
+            }
+            
+            current = current.map.get(item);
+        }
+        
+        return true;
+        
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
