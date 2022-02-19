75b739721a650110efaa0e62ea6d27efcbc439df
Mongolian <abe546@live.com>
2022-02-19 04:44:04 -0500

    Time: 0 ms (100.00%), Space: 54.4 MB (14.02%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int start = 0; 
+        int end = nums.length-1;
+        int middle = (end - start + 1)/2;
+            
+            while(start <= end && end >= 0 && start < nums.length)
+            {
+                middle = (end - start)/2 + start;
+                if(nums[middle] > target)
+                {
+                    end = middle-1; 
+                }else if(nums[middle] < target)
+                {
+                    start = middle+1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+        
+        return -1; 
+        
+    }
+}
