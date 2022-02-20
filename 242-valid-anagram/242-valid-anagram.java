71caa12b36ec30010774529ed4725dab2c6b60ae
Mongolian <abe546@live.com>
2022-02-20 05:39:03 -0500

    Time: 33 ms (9.17%), Space: 46.7 MB (6.21%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -6,28 +6,27 @@ class Solution {
             return false; 
         }
         
-        Map<Character, Integer> map1 = new HashMap(); 
-        Map<Character, Integer> map2 = new HashMap(); 
+        Map<Integer, Integer> map1 = new HashMap(); 
+        Map<Integer, Integer> map2 = new HashMap(); 
         
         Set<Character> set = new HashSet(); 
         
         for(int i = 0; i < s.length(); i++)
         {
-            map1.put(s.charAt(i), 
-                    map1.getOrDefault(s.charAt(i), 0)+1); 
+            map1.put(s.charAt(i)-'a', 
+                    map1.getOrDefault(s.charAt(i)-'a', 0)+1); 
             
-            map2.put(t.charAt(i), 
-                    map2.getOrDefault(t.charAt(i), 0)+1); 
+            map2.put(t.charAt(i)-'a', 
+                    map2.getOrDefault(t.charAt(i)-'a', 0)+1); 
             
             set.add(s.charAt(i));
             set.add(t.charAt(i)); 
         }
        
   
-        
         for(char entry : set)
         {
-            if(map1.getOrDefault(entry, 0).intValue() != map2.getOrDefault(entry, 0).intValue())
+            if(map1.getOrDefault(entry-'a', 0).intValue() != map2.getOrDefault(entry-'a', 0).intValue())
             {
                 return false; 
             }
