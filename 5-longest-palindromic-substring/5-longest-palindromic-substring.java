d45f3033ae2f1b89c5c02bfe95a0b0c09160ce0f
Mongolian <abe546@live.com>
2022-02-27 05:17:14 -0500

    Time: 42 ms (71.64%), Space: 43.1 MB (55.97%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,25 +1,64 @@
 class Solution {
     public String longestPalindrome(String s) {
-    if (s == null || s.length() < 1) return "";
-    int start = 0, end = 0;
-    for (int i = 0; i < s.length(); i++) {
-        int len1 = expandAroundCenter(s, i, i);
-        int len2 = expandAroundCenter(s, i, i + 1);
-        int len = Math.max(len1, len2);
-        if (len > end - start) {
-            start = i - (len - 1) / 2;
-            end = i + len / 2;
+     
+        if(s.length() < 2)
+        {
+            return s; 
         }
-    }
-    return s.substring(start, end + 1);
-}
+        
+        String longest = ""+s.charAt(0);
+  
+        int start = 0; 
+        int end = 0; 
+        for(int i = 0; i < s.length(); i++)
+        { 
+   
+                 int iter = i;
+                int iterR = i;
+                while(iter >= 0 && iterR < s.length() && s.charAt(iter) == s.charAt(iterR))
+                { 
+                    iter--;
+                    iterR++;
+   
+                }
+ 
+            if(end - start < iterR - iter - 1)
+            {
+                int len = iterR - iter - 1;
+               start = i - (len - 1) / 2;
+            end = i + len / 2;
+            }
+ 
+        }
+ 
+                for(int i = 0; i < s.length(); i++)
+        { 
+                    
+       
+                int iter = i;
+                int iterR = i+1;
+ 
+ 
+                while(iter >= 0 && iterR < s.length() && s.charAt(iter) == s.charAt(iterR))
+                {
+             
+                    iter--;
+                    iterR++;
+    
+                }
+ 
+     
+                 if(end - start < iterR - iter - 1)
+            {
+                      int len = iterR - iter - 1;
+                start = i - (len - 1) / 2;
+            end = i + len / 2;
+            }    
+                    
+        }
+        
+        
 
-private int expandAroundCenter(String s, int left, int right) {
-    int L = left, R = right;
-    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-        L--;
-        R++;
+        return s.substring(start, end+1);
     }
-    return R - L - 1;
-}
 }
