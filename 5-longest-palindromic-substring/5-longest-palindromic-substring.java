2af9ba1b4a5a12c5f56b3a3156d410544f608f50
Mongolian <abe546@live.com>
2022-02-27 20:49:10 -0500

    Time: 24 ms (91.49%), Space: 48 MB (40.19%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,57 +1,54 @@
 class Solution {
     public String longestPalindrome(String s) {
     
+    
         if(s.length() <= 1)
         {
-            return s;
+            return s; 
         }
         
-        char[] arr = s.toLowerCase().toCharArray();
+        char[] arr = s.toCharArray();
         
         int start = 0; 
-        int end = 0; 
+        int end = 0;
         
         for(int i = 0; i < arr.length; i++)
         {
-            int[] result = findPalindrome( i, i, arr);            
-           int[] result2 = findPalindrome( i, i+1, arr);
+            int[] resultA = palindrome(i, i, arr); 
+            int[] resultB = palindrome(i, i+1, arr); 
             
-            int len1 = result[1] - result[0];
-            int len2 = result2[1] - result2[0];
-            int ansLen = end - start ; 
-            
-            if(ansLen < len1)
+            if(end - start < resultA[1] - resultA[0])
             {
-                start = result[0];
-                end = result[1];
-                
+                end = resultA[1]; 
+                start = resultA[0];
             }
             
-            if(ansLen < len2)
+            if(end - start < resultB[1] - resultB[0])
             {
-                 start = result2[0];
-                end = result2[1];
+                end = resultB[1]; 
+                start = resultB[0]; 
             }
-            
-        
-            
         }
         
-        return s.substring(start, end+1); 
+        return s.substring(start, end + 1);
     }
     
-    public int[] findPalindrome( int left, int right, char[] arr)
+    /**
+    * Method to find a palindrome's start and end index
+    */
+    public int[] palindrome(int left, int right, char[] arr)
     {
-         int[] answer = {0,0};
- 
+        int[] answer = {0, 0};
         
-        while( left >= 0 &&  right < arr.length && arr[left] == arr[right])
+        while(left >= 0 && right < arr.length && arr[left] == arr[right])
         {
-                 answer[0] =  left--;
-                  answer[1] =  right++;
-   
+            answer[0] = left;
+            answer[1] = right;
+            
+            left--;
+            right++;
         }
-                
+        
         return answer;
     }
 }
