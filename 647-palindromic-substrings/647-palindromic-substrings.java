d92c5dae87f831ec6ef8330b008a035e62ffeedc
Mongolian <abe546@live.com>
2022-02-27 21:33:48 -0500

    Time: 904 ms (5.03%), Space: 168.3 MB (5.05%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,6 +1,6 @@
 class Solution {
    //Set of start and end times (index 0 is start, index 1 is end)
- 
+   private Set<List<Integer>> set = new HashSet(); 
     public int countSubstrings(String s) {
       
         if(s.length() <= 1)
@@ -9,31 +9,41 @@ class Solution {
         }
         
         char[] arr = s.toCharArray();
-        int count = 0; 
+   
+        
+        ArrayList<Integer> init = new ArrayList();
+        init.add(0);
+        init.add(0);
+        
+        set.add(init);
+        
         for(int i = 0; i < arr.length; i++)
         {
-            for(int j = i; j < arr.length; j++)
-            {
-                StringBuilder sb = new StringBuilder(); 
-                String tmp = s.substring(i,j+1);
-                sb.append(tmp);
-                
-                if(isPalindrome(tmp, sb))
-                {
-                    count++;
-                }
-            }
+            palindrome(i, i, arr); 
+            palindrome(i, i+1, arr); 
         }
         
-        return count; 
-    }
-    /**
-    * Given a String and a StringBuilder return true if they're a palindrome of each other
-    */
-    public boolean isPalindrome(String origin, StringBuilder compare)
-    {
-        return origin.equals(compare.reverse().toString()); 
+ 
+        
+        return   set.size(); 
     }
     
- 
+    /**
+    * Find all possible palindromes from point given. Iterate left and right, until arr[left] != arr[right]
+    */
+    public void palindrome(int left, int right, char[] arr)
+    {        
+        while(left >= 0 && right < arr.length && arr[left] == arr[right])
+        {
+            ArrayList<Integer> tmp = new ArrayList();
+            
+            tmp.add(left);
+            tmp.add(right);
+            
+            set.add(tmp);
+            
+            left--;
+            right++;
+        }
+    }
 }
