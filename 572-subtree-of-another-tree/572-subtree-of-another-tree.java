83241f997b0a0652a429c764795eac6b9eb6bfd3
Mongolian <abe546@live.com>
2022-04-16 05:13:23 -0400

    Time: 3 ms (97.39%), Space: 47 MB (20.22%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -14,65 +14,69 @@
  * }
  */
 class Solution {
- 
+   Queue<TreeNode> que = new LinkedList(); 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         
         if(root == subRoot && root == null)
         {
             return true; 
         }
- 
-        Stack<TreeNode> stack = new Stack(); 
-        stack.add(root); 
+  
         
-        while(!stack.isEmpty())
+        findHead(root, subRoot); 
+    
+         while(!que.isEmpty())
         {
-            TreeNode current = stack.pop(); 
-            
-            if(current == null)
-            {
-                continue; 
-            }
-            
-            if(current.val == subRoot.val)
-            {
-            if(isSubTree(current, subRoot))
+            if(isParent(que.poll(), subRoot))
             {
                 return true; 
             }
-            }
-            
-            stack.add(current.right); 
-            stack.add(current.left); 
         }
-    
         
         return false; 
     }
     
-   public boolean isSubTree(TreeNode root, TreeNode subRoot)
-    {
-        boolean flag = false; 
+    public void findHead(TreeNode root, TreeNode target)
+    {        
+        if(root == null)
+        {
+            return;
+        }
         
-        if(root == null && subRoot == null)
+         if(target.val == root.val)
         { 
-            flag = true;
+            que.add(root); 
+        }
+        
+       findHead(root.left, target); 
+  
+        findHead(root.right, target); 
+ 
+    }
+    
+    public boolean isParent(TreeNode root, TreeNode subRoot)
+    {
+        if(subRoot == null && root == null)
+        {
             return true; 
         }
         
-        if(root != null && subRoot != null && root.val == subRoot.val)
-        { 
-            flag = true; 
-        }
-        
-        if(flag != true || ((root == null && subRoot != null) || (root != null && subRoot == null)))
-        { 
+        if(subRoot == null || root == null)
+        {
             return false; 
         }
- 
-        flag = isSubTree(root.left, subRoot.left) && isSubTree(root.right, subRoot.right);
         
-        return flag; 
+        boolean flag = false; 
+        
+        if(subRoot.val == root.val)
+        {
+            flag = true;
+        }else
+        {
+            return false;
+        }
+        
+        return flag && isParent(root.left, subRoot.left) && isParent(root.right, subRoot.right); 
     }
      
 }
