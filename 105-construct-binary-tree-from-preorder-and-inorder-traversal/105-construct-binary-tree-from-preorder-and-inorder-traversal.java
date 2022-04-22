6d7671129b75709bbd9680e7e5288d6f71fb3293
Mongolian <abe546@live.com>
2022-04-22 06:22:17 -0400

    Time: 3 ms (81.94%), Space: 44.7 MB (33.14%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,96 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
- 
-    HashMap<Integer, Integer> inorderIndex = new HashMap(); 
-    TreeMap<Integer, Integer> exist = new TreeMap(); 
+    int preorderIndex;
+    Map<Integer, Integer> inorderIndexMap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-  
-        Queue<Integer> que = new LinkedList(); 
-        
-        for(int i = 0; i < inorder.length; i++)
-        {
-            inorderIndex.put(inorder[i], i); 
-            que.add(preorder[i]); 
+        preorderIndex = 0;
+        // build a hashmap to store value -> its index relations
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
         }
-        
-        return construct(que, inorder);
+
+        return arrayToTree(preorder, 0, preorder.length - 1);
     }
-    
-    public TreeNode construct(Queue<Integer> que, int[] inorder)
-    {
-        Integer current = que.poll(); 
+
+    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+        // if there are no elements to construct the tree
+        if (left > right) return null;
+
+        // select the preorder_index element as the root and increment it
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+ 
         
-        if(current == null)
-        {
-            return null; 
-        }
-        
-        TreeNode root = new TreeNode(current.intValue()); 
-        exist.put(inorderIndex.get(root.val), root.val);
-        
-        root.left = null;
-        root.right = null; 
-        
-        if(que.peek() != null && canBeLeft(que.peek(), root))
-        {
-            root.left = construct(que, inorder); 
-        }
-        
-        if(que.peek() != null && canBeRight(que.peek(), root, inorder))
-        {
-            root.right = construct(que, inorder); 
-        }
-        
-        return root; 
+        // build left and right subtree
+        // excluding inorderIndexMap[rootValue] element because it's the root
+        root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+        return root;
     }
-    
-    public boolean canBeRight(Integer candidate, TreeNode root, int[] inorder)
-    {
-        int candidateIndex = inorderIndex.get(candidate.intValue()).intValue(); 
-        int rootIndex = inorderIndex.get(root.val).intValue(); 
-        
-        if(rootIndex > candidateIndex)
-        {
-            return false; 
-        }
-        
-        Integer nextIndex = exist.ceilingKey(rootIndex+1); 
-        
-        if(nextIndex != null &&
-          nextIndex != candidateIndex && 
-          nextIndex < candidateIndex)
-        {
-            return false; 
-        }
-        
-        return true; 
-    }
-    
-    public boolean canBeLeft(Integer candidate, TreeNode root)
-    {
-        int candidateIndex = inorderIndex.get(candidate.intValue()).intValue(); 
-        int rootIndex = inorderIndex.get(root.val).intValue(); 
-        
-        if(rootIndex < candidateIndex)
-        {
-            return false;
-        }
-        
-        return true; 
-    }
-       
 }
