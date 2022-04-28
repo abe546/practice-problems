f55c8b65e0ebd71fe7b91add9b951349dab519ff
Mongolian <abe546@live.com>
2022-04-27 21:46:07 -0400

    Time: 4 ms (100.00%), Space: 43.4 MB (79.08%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,46 +9,79 @@
  */
 
 class Solution {
-    Map<TreeNode, TreeNode> parentMap = new HashMap(); 
-    Set<TreeNode> set = new HashSet();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        traverse(root, null); 
+        //Find all parents of each node
+     /*   
+        Map<Integer, TreeNode> parentsOfP = new HashMap(); 
+        Map<Integer, TreeNode> parentsOfQ = new HashMap(); 
+        //Assuming BST standard so that all nodes are unique.
         
-        TreeNode pointer = p;
-                
-        while(pointer != null)
-        {
-            set.add(pointer); 
+        findParents(root, p, null, parentsOfP); //null represents the parent of root which is null
+        findParents(root, q, null, parentsOfQ); //null represents the parent of root which is null
+        
+        //Get direct parents of P (including self)
+        
             
-            pointer = parentMap.get(pointer); 
+        TreeNode currP = p;
+        TreeNode currQ = q;
+        
+        Set<Integer> parentsOfPSet = new HashSet(); 
+        
+        while(currP != null)
+        {
+            parentsOfPSet.add(currP.val); 
+            
+            currP = parentsOfP.get(currP.val); 
         }
         
-        pointer = q;
-        
-        while(pointer != null)
+        while(currQ != null)
         {
-            if(set.contains(pointer))
+            if(parentsOfPSet.contains(currQ.val))
             {
-                return pointer;
+                break; 
             }
             
-            pointer = parentMap.get(pointer);
+            currQ = parentsOfQ.get(currQ.val); 
+        }
+    */
+        
+        int parentVal = root.val;
+        
+        if(q.val > parentVal && p.val > parentVal)
+        {
+             return lowestCommonAncestor( root.right, p, q); 
+        }else if(q.val < parentVal && p.val < parentVal)
+        {
+            return lowestCommonAncestor( root.left, p, q); 
+        }else
+        {
+            //At this point root is either equal to p.val or q.val in either case we return current node
+            //OR this is the point where there is an inflection and the two nodes will no longer will be in the same subtree
+            //So we simply return root as this willbe the last time they share a parent. 
+            return root;
         }
         
-        return root;    
+ 
     }
     
-    public void traverse(TreeNode root, TreeNode parent)
+    
+    
+    public void findParents(TreeNode root, TreeNode child, TreeNode parent, Map<Integer, TreeNode> parentsOfChild)
     {
         if(root == null)
         {
             return;
         }
         
-        parentMap.put(root, parent); 
+        parentsOfChild.put(root.val, parent); 
         
-        traverse(root.left, root); 
-        traverse(root.right, root); 
+        if(child.val == root.val)
+        {
+            return;
+        }
+    
+        findParents(root.left, child, root, parentsOfChild);
+        findParents(root.right, child, root, parentsOfChild);
     }
 }
