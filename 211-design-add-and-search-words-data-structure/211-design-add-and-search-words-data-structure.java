a82c14dae71d9549cab8059d290b088be98d9416
Mongolian <abe546@live.com>
2022-05-05 01:15:45 -0400

    Time: 2357 ms (5.94%), Space: 271.9 MB (38.10%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,80 @@
+class WordDictionary {
+ 
+    private Map<Character, WordDictionary> dict = new HashMap();
+    private boolean endOfWord = false; 
+    private final static char WILD_CARD = '.';
+    
+    public void addWord(String word) {
+ 
+        WordDictionary current = this; 
+        char[] arr = word.toCharArray();
+        
+        for(char item : arr)
+        {
+            WordDictionary wordDict = current.dict.get(item);
+            
+            if(wordDict == null)
+            {
+                wordDict = new WordDictionary(); 
+                current.dict.put(item, wordDict); 
+            }
+            
+            current = current.dict.get(item); 
+        }
+        
+        current.endOfWord = true; 
+    }
+    
+    public boolean search(String word) {
+ 
+        return search(word, this); 
+    }
+    
+    private boolean search(String word, WordDictionary context)
+    { 
+        WordDictionary current = context; 
+        char[] arr = word.toCharArray();
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            char item = arr[i];
+          
+            if(item == WILD_CARD)
+            {
+                for(char key : current.dict.keySet()){
+                
+                WordDictionary pointer = current.dict.get(key); 
+                
+                boolean result = search(word.substring(i+1, word.length()), pointer); 
+                
+                if(result)
+                {
+                    return result;
+                }
+                }
+            }
+            
+            WordDictionary wordDir = current.dict.get(item);
+            
+            if(wordDir == null)
+            {
+                return false; 
+            }
+            
+            current = current.dict.get(item);
+        }
+        
+        return current.endOfWord;
+    }
+    
+     
+    
+     
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
