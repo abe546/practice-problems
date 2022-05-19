d79d832ecd4db825bc6b2a26c9e192fae732e6e1
Mongolian <abe546@live.com>
2022-05-19 00:00:34 -0400

    Time: 116 ms (11.15%), Space: 44.3 MB (49.13%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -6,19 +6,22 @@ class Solution {
         Arrays.fill(sequence, 1); 
         int max = 1; 
         
+        // We fill with 1 to signify that the longest sequence is at least 1 
+        // (the current item)
+        
         for(int i = 0; i < nums.length; i++)
         {
             for(int j = 0; j < i; j++)
-            {
+            {                
                 if(nums[j] < nums[i])
                 {
-                    sequence[i] = Math.max(sequence[i] , sequence[j]+1);
+                    sequence[i] = Math.max(sequence[i], sequence[j]+1);
+                    
                     max = Math.max(sequence[i], max);
                 }
             }
         }
         
         return max; 
-        
     }
 }
