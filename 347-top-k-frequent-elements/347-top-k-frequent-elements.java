a433b3a57dd0dd5b7541a2b88746a00b3a8b7674
Mongolian <abe546@live.com>
2022-07-09 00:04:35 -0400

    Time: 22 ms (36.95%), Space: 50.1 MB (72.75%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,57 +1,34 @@
 class Solution {
     
-    private Set<Integer> uniqueItems = new HashSet(); 
     public int[] topKFrequent(int[] nums, int k) {
     
-        Map<Integer, Integer> numberCount = new HashMap(); 
+        Map<Integer, Integer> numberToCount = new HashMap(); 
         
-        populateMap(numberCount, nums);
-        
-        // MAX Heap
-        PriorityQueue<Integer> heap = 
-            new PriorityQueue<>(
-        (a,b) ->
-            {
-                int aValue = numberCount.get(a).intValue(); 
-                int bValue = numberCount.get(b).intValue(); 
-          
-                if(aValue > bValue)
-                {
-                    return -1000;
-                }else if(bValue > aValue)
-                {
-                    return 1;
-                }
-                
-                return 0;
-                
-            }
-        );
-        
-        for(int item : uniqueItems)
-        {
-            heap.add(item); 
-        }
-        
-        int[] answer = new int[k]; 
-        int i = 0; 
-        while(!heap.isEmpty() && i < k){
-        answer[i++] = heap.poll(); 
-        }
-        
-        return answer; 
-    }
-    
-    public void populateMap(Map<Integer, Integer> numberCount, int[] nums)
-    {
         for(int i = 0; i < nums.length; i++)
         {
-            int count = numberCount.getOrDefault(nums[i], 0).intValue();
-            count++; 
-            
-            numberCount.put(nums[i], count); 
-            
-            uniqueItems.add(nums[i]); 
+            int count = numberToCount.getOrDefault(nums[i], 0); 
+            count++;
+            numberToCount.put(nums[i], count); 
         }
+        
+        PriorityQueue<Integer> que = new PriorityQueue<>
+            ((a,b)
+            ->{
+               return numberToCount.get(b) - numberToCount.get(a);  
+            });
+        
+        for(int item : numberToCount.keySet())
+        {
+            que.add(item); 
+        }
+        int[] arr = new int[k];
+        for(int i = 0; i < k; i++)
+        {
+            arr[i] = que.poll(); 
+        }
+        
+        return arr; 
     }
+    
+    
 }
