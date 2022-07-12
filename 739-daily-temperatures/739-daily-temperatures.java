ddec03073c69176526dd76ca8812d8ef25e2f64d
Mongolian <abe546@live.com>
2022-07-12 03:54:12 -0400

    Time: 148 ms (60.47%), Space: 129.5 MB (52.64%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,34 +1,22 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-         
-        Map<Integer, Integer> temps= new LinkedHashMap<>();
-        int[] answers = new int[temperatures.length];
+    public int[] dailyTemperatures(int[] T) {
+      int[] answer = new int[T.length];
         
-        for(int i = temperatures.length-1; i >=0; i--)
+        Stack<Integer> indexes = new Stack();
+        
+        for(int i = 0; i < T.length; i++)
         {
-            int current = temperatures[i];
-            temps.put(current, i);
-            // Define as zero by default
-            answers[i] = 0;
-    
-            for(Integer entry : temps.keySet())
-            {  
-                
-                if(entry.intValue() > temperatures[i])
-                { 
-                    if(answers[i] == 0)
-                    {
-                        answers[i] = temps.get(entry.intValue()) - i;
-                    }
-                    
-                    answers[i] = Math.min(
-                        temps.get(entry.intValue()) - i, answers[i]);
-               
-                }
-            }
             
+                while(!indexes.isEmpty() && T[indexes.peek()] < T[i])
+                {
+                    int index = indexes.pop();
+                    answer[index] = i - index;
+                }
+                indexes.push(i);
+         
         }
+ 
         
-        return answers; 
+        return answer; 
     }
 }
