2ee184000f6614cbbeb23daa67e4bc7ef2203f08
Mongolian <abe546@live.com>
2022-07-13 02:00:14 -0400

    Time: 19 ms (21.00%), Space: 51.3 MB (11.14%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,74 +1,68 @@
 class Solution {
-    
-    private     Map<Integer, Boolean> map = new HashMap(); 
  
+    private Map<Integer, Set<Integer>> graph = new HashMap<>();    
+    private Set<Integer> cycle = new HashSet<>(); 
+    private Map<Integer, Boolean> visited = new HashMap(); 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-     
-    Map<Integer, Set<Integer>> graph = new HashMap();  
-    Set<Integer> memory = new HashSet<Integer>();
-        
-
-        
-    createGraph(graph, prerequisites);
-        
-        for(int[] items : prerequisites)
-        {
-     
-            if(!allowed(items[0], memory, graph)){
-                return false; 
-            }
  
-        }
-    
-    return true;     
+        
+       populateGraph(prerequisites);
+         
+       for(int node : graph.keySet())
+       {
+           cycle.clear(); 
+           if(allowed(node) != true)
+           {
+               return false; 
+           }
+       }
+        
+       return true;  
     }
     
-    public boolean allowed(int node, Set<Integer> memory, Map<Integer, Set<Integer>> graph)
-    {
-        if(map.get(node) != null)
+    public boolean allowed(int node)
+    {   
+        if(visited.get(node) != null)
         {
-            return map.get(node); 
+            return visited.get(node); 
         }
         
-        if(memory.contains(node))
-        {
-            return false; 
-        }
-        
-        if(graph.get(node) == null)
-        {
-            //no children
-            return true; 
-        }
-        
-        memory.add(node); 
+        if(graph.get(node) != null)
+        {               
+        cycle.add(node); 
         
         for(int child : graph.get(node))
         {
-            if(!allowed(child, memory, graph))
+            if(cycle.contains(child))
+            { 
+                visited.put(node, false); 
+                return false; 
+            }
+            
+            if(allowed(child) != true)
             {
+                visited.put(node, false); 
                 return false; 
             }
         }
-  
- 
-        map.put(node, true);
         
+        cycle.remove(node); 
+        }
+        visited.put(node, true); 
         return true; 
     }
     
-    public void createGraph(Map<Integer, Set<Integer>> graph, int[][] prereqs)
+    public void populateGraph(int[][] matrix)
     {
-        for(int i = 0; i < prereqs.length; i++)
+        for(int i = 0; i < matrix.length; i++)
         {
-            //Given preres[0-n].length == 2
-            int from = prereqs[i][0];
-            int to = prereqs[i][1]; 
+            int source = matrix[i][0];
+            int destination = matrix[i][1]; 
             
-            Set<Integer> tmp = graph.getOrDefault(from, new HashSet<Integer>());
-            tmp.add(to); 
+            Set<Integer> nodes = graph.getOrDefault(source, new HashSet<>());    
+            nodes.add(destination); 
             
-            graph.put(from, tmp); 
+            graph.put(source, nodes); 
         }
     }
 }
