85b20040a4f4fb943c73733f438734d5579d1e72
Mongolian <abe546@live.com>
2022-08-23 14:25:57 -0400

    Time: 32 ms (11.95%), Space: 50.6 MB (45.15%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,34 +1,43 @@
 class Solution {
-    
     public int[] topKFrequent(int[] nums, int k) {
-    
-        Map<Integer, Integer> numberToCount = new HashMap(); 
         
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> uniqueNums = new HashSet<>();
         for(int i = 0; i < nums.length; i++)
         {
-            int count = numberToCount.getOrDefault(nums[i], 0); 
+            int count = map.getOrDefault(nums[i], 0);
             count++;
-            numberToCount.put(nums[i], count); 
+            map.put(nums[i], count);
+            uniqueNums.add(nums[i]); 
         }
         
-        PriorityQueue<Integer> que = new PriorityQueue<>
-            ((a,b)
-            ->{
-               return numberToCount.get(b) - numberToCount.get(a);  
-            });
+        PriorityQueue<Integer> que = new PriorityQueue<>(
+        (a,b) ->
+            {
+                if(map.get(a) > map.get(b))
+                {
+                    return -1;
+                }else if(map.get(b) > map.get(a))
+                {
+                    return 1;
+                }
+                
+                return 0; 
+            }
+        );
         
-        for(int item : numberToCount.keySet())
+        for(int item : uniqueNums)
         {
             que.add(item); 
         }
+        
         int[] arr = new int[k];
+        
         for(int i = 0; i < k; i++)
         {
-            arr[i] = que.poll(); 
+            arr[i] = que.poll();
         }
         
         return arr; 
     }
-    
-    
 }
